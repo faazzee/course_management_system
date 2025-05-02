5ff1ae656c02e8dd804e36bc2128a24752133de1
vignesh <145251963+faazzee@users.noreply.github.com>
2025-05-02 17:47:20 +0800

    Delete collage sys directory

--- collage sys/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from controllers.courses_router import course_router
-from controllers.enrollments_router import enrollment_router
-from controllers.students_router import student_router
-
-app=FastAPI()
-app.include_router(course_router,prefix='/api',tags=['users'])
-app.include_router(enrollment_router,prefix='/api',tags=['users'])
-app.include_router(student_router,prefix='/api',tags=['users'])
